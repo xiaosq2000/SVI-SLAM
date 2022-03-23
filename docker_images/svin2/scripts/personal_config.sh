5ce502d6d32f2e72a3769c0f850c6360fa946505
Shuqi Xiao <xiaosq2000@gmail.com>
2022-03-23 22:01:28 +0800

    chores: delete duplicate files

--- docker_images/svin2/scripts/personal_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git config --global user.name "Shuqi Xiao"
-git config --global user.email "xiaosq2000@gmail.com"
-echo "source /opt/ros/kinetic/setup.bash" >> /root/.bashrc
-echo "source /usr/app/svin2_ws/devel/setup.bash" >> /root/.bashrc
